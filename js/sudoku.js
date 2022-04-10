5fa3e98b9f0863dda5d19fef7722c7c07087bbad
Enrique Jose Giron Tomas <68545195+qquiquee@users.noreply.github.com>
2022-04-10 16:19:19 +0200

    otro

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,14 +1,14 @@
 const grid = document.getElementById("grid");
 
-for(let i = 1; i < 5; i++)
+for(let i = 1; i < 9; i++)
 {
-    for(let x = 1; x < 5; x++)
+    for(let x = 1; x < 9; x++)
     {
         var item = document.createElement("div");
         //item.setAttribute("style", "grid-area:" + i + " / " + x + " / " + i + " / " + x);
         item.setAttribute("grid-column", x + " / span 1");
         item.setAttribute("grid-row", i + " / span 1");
-        item.setAttribute("class", "grid-item");
+        item.setAttribute("id", "grid-item");
         grid.appendChild(item);
     }
 }
