67faa19b51aafeb313aa8c9ac80ff412acdd40cf
Enrique Jose Giron Tomas <68545195+qquiquee@users.noreply.github.com>
2022-04-10 16:17:01 +0200

    otro

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,2 +1,14 @@
-let dale = 'Fourbo';
-alert(`Llamando a ${dale}`);
+const grid = document.getElementById("grid");
+
+for(let i = 1; i < 5; i++)
+{
+    for(let x = 1; x < 5; x++)
+    {
+        var item = document.createElement("div");
+        //item.setAttribute("style", "grid-area:" + i + " / " + x + " / " + i + " / " + x);
+        item.setAttribute("grid-column", x + " / span 1");
+        item.setAttribute("grid-row", i + " / span 1");
+        item.setAttribute("class", "grid-item");
+        grid.appendChild(item);
+    }
+}
